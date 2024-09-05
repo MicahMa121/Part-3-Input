b6bcccbd035e3715b287b88a610e0a6e9e2690c6
Micah Ma <701004018@GoTVDSB.ca>
2024-09-05 09:40:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+
+namespace Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, What is Your Name?");
+            string name = Console.ReadLine();
+            int age;
+            int year;
+            do
+            {
+                Console.WriteLine(name + ", How Old Are You?");
+            }
+            while (!int.TryParse(Console.ReadLine(), out age));
+            do
+            {
+                Console.WriteLine(name + ", What is the Current Year?");
+            }
+            while (!int.TryParse(Console.ReadLine(), out year));
+        }
+    }
+}
